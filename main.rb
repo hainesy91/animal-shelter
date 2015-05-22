4c0dd343f33fe15f1e597b84575cc4c082d7fc90
Christopher Haines <hainesy91@gmail.com>
2015-05-22 12:12:27 +0100

    required relative to all of the files from within the main file, tried writing code for the menu with the different options

--- main.rb
+++ main.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
-require_relative "Clients"
-require_relative "Animals"
-require_relative "Shelter"
+require_relative "clients"
+require_relative "animal"
+require_relative "shelter"
+
+def menu
+  puts 'clear'
+  puts "-" * 80
+  puts "List of available animals (a) \nList of potential clients (b) \nCreate an animal (c) \nCreate a client (d) \nAdopt Animal (e) \nReturn an animal (f) \nQuit (q)"
+  puts "-" * 80
+  gets.chomp
+end
+
+binding.pry
